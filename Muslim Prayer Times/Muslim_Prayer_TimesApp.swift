3c348e1e598b9d52489eb3c9e9381cdc13b2f7cf
Ikhwan <nsetyo19@gmail.com>
2026-04-18 13:45:09 +0700

    Initial Commit

---
+++ Muslim Prayer Times/Muslim_Prayer_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muslim_Prayer_TimesApp.swift
+//  Muslim Prayer Times
+//
+//  Created by Ikhwan Setyo on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Muslim_Prayer_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
